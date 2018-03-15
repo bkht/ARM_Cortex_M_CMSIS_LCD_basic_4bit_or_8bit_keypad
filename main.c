838de01f463aeec8a64a68ec1976381b5df2b2fb
bkht <info@jacksoft.nl>
2018-03-16 00:47:59 +0100

    first commit

--- main.c
+++ main.c
@@ -8,9 +8,37 @@
 // PORTE
 // 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
 // D7 D6 D5 D4 D3 D2 D1 D0 RW RS  E
+// PORTD KEYPAD, LEDs
+// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+// LB LR LO LG C3 C2 C1 C0       LE    R3 R2 R1 R0
 
 // Use the display in 4-bit mode, otherwise use it in 8-bit mode
-//#define DISPLAY_4_BIT_MODE
+#define DISPLAY_4_BIT_MODE
+#define DISPLAY_SIZE_X 20
+#define DISPLAY_SIZE_Y  4
+
+// Keypad connections
+// Top view, bottom 1-8 from left to right:
+//  1   2   3   4   5   6   7   8
+// C0  C1  C2  C3  R0  R1  R2  R3
+// D8  D9  D10 D11 D0  D1  D2  D3
+// IN  IN  IN  IN  OUT OUT OUT OUT
+// Keys:
+// C0 C1 C2 C3
+//  1  2  3  A  R0
+//  4  5  6  B  R1
+//  7  8  9  C  R2
+//  *  0  #  D  R3
+
+#define KEYPAD GPIOD
+#define K_R0  (1U << 0)
+#define K_R1  (1U << 1)
+#define K_R2  (1U << 2)
+#define K_R3  (1U << 3)
+#define K_C0  (1U << 8)
+#define K_C1  (1U << 9)
+#define K_C2  (1U << 10)
+#define K_C3  (1U << 11)
 
 #define LED_LD8_R (1U<<5)
 #define LED_LD4_G (1U<<12)
@@ -39,19 +67,32 @@ void LCD_Data(uint8_t data);
 void LCD_Write(uint8_t data, uint8_t control);
 
 void LCD_GotoXY(uint8_t x, uint8_t y);
+void LCD_Char(uint8_t character);
 void LCD_String(char *str);
+void LCD_Increment_Cursor_Position(void);
 void LCD_Clear(void);
+
+uint8_t getKey(void);
 
 void GPIO_Init(void);
 
 void delay_ms(uint16_t n);
 void delay_us(uint32_t n);
 
+typedef struct cursor_position { 
+    uint8_t x; 
+    uint8_t y; 
+} cursor_position;
+
+cursor_position cp;
+
 int main()
 {
-	uint16_t counter = 0;
-	char buffer[10];
-
+	uint8_t key = 0;
+	uint8_t key_old = 0;
+	
+	GPIO_Init();
+	
 	LCD_Init();
 
 	GPIOD->BSRR = (LED_LD5_R | LED_LD4_G | LED_LD6_B);
@@ -61,30 +102,35 @@ int main()
 	LCD_GotoXY(0,0);
 	LCD_String("Hello");
 	
-	LCD_GotoXY(2,1);
+	LCD_GotoXY(3,1);
 	LCD_String("Display");
 
-	LCD_GotoXY(4,2);
+	LCD_GotoXY(6,2);
 	LCD_String("in");
 	
-	LCD_GotoXY(6,3);
+	LCD_GotoXY(9,3);
 #ifdef DISPLAY_4_BIT_MODE
 	LCD_String("4-bit mode");
 #else
 	LCD_String("8-bit mode");
 #endif
+	LCD_GotoXY(0,0);
 	while (1) {
-		LCD_GotoXY(10,0);
-		sprintf(buffer, "%5d", counter);
-		LCD_String(buffer);
-		counter++;
-		delay_ms(1000);
+		key = getKey();
+		if ((key != 0) && (key_old == 0)) {
+			//LCD_GotoXY(15,0);
+			LCD_Char(key);
+		}
+		key_old = key;
+		
 	}
 }
 
 void LCD_GotoXY(uint8_t x, uint8_t y)
 {
 	uint8_t address = 0;
+	cp.x = x;
+	cp.y = y;
 	
 	switch (y) {
 		case 1:
@@ -102,10 +148,31 @@ void LCD_GotoXY(uint8_t x, uint8_t y)
 	LCD_Cmd(address | 0x80);
 }
 
+void LCD_Char(uint8_t character)
+{
+	LCD_Data(character);
+	LCD_Increment_Cursor_Position();
+}
+
 void LCD_String(char *str)
 {
 	while(*str) {
 		LCD_Data(*str++);
+		LCD_Increment_Cursor_Position();
+	}
+}
+
+void LCD_Increment_Cursor_Position(void)
+{
+	cp.x++;
+	if (cp.x == DISPLAY_SIZE_X) {
+		cp.x = 0;
+		cp.y++;
+		LCD_GotoXY(cp.x, cp.y);
+		if (cp.y == DISPLAY_SIZE_Y) {
+			cp.y = 0;
+			LCD_GotoXY(cp.x, cp.y);
+		}
 	}
 }
 
@@ -113,6 +180,8 @@ void LCD_Clear(void)
 {
 	LCD_Cmd(0x01);	// Display clear
 	LCD_Cmd(0x80);	// Set DDRAM address to 0
+	cp.x = 0;
+	cp.y = 0;
 }
 
 void LCD_Init(void)
@@ -127,11 +196,6 @@ void LCD_Init(void)
 
 	// Wait for power stabilization: >= 1ms
 	delay_ms(2);
-
-    // Enable clock to GPIO Port
-    // Set GPIO pins as output pins
-    // Enable GPIO pins
-	GPIO_Init();
 	
 #ifdef DISPLAY_4_BIT_MODE
 	// Use the display in 4-bit mode
@@ -162,6 +226,7 @@ void LCD_Init(void)
 	
 	LCD_Cmd(0x80);	// 10000000 Set DDRAM address to 0
 
+	LCD_Clear();
 }
 
 void LCD_Cmd(uint8_t command) {
@@ -244,53 +309,189 @@ void LCD_Write(uint8_t data, uint8_t control) {
 //	GPIOE->BSRR = (HD44780_RS << 16); // reset RS=0
 }
 
+uint8_t getKey(void) {
+	// Keypad connections
+	// Top view, bottom 1-8 from left to right:
+	//  1   2   3   4   5   6   7   8
+	// C0  C1  C2  C3  R0  R1  R2  R3
+	// D8  D9  D10 D11 D0  D1  D2  D3
+	// IN  IN  IN  IN  OUT OUT OUT OUT
+	// Keys:
+	// C0 C1 C2 C3
+	//  1  2  3  A  R0
+	//  4  5  6  B  R1
+	//  7  8  9  C  R2
+	//  *  0  #  D  R3
+	// GPIOD 0000 CCCC 0000 RRRR
+	//            3210      3210
+	const uint8_t keymap[4][4]={
+		{'1','2','3','A'},
+		{'4','5','6','B'},
+		{'7','8','9','C'},
+		{'*','0','#','D'}
+	};
+	uint8_t k_row, k_col;
+	// Clear bits for R0-R3=0
+	GPIOD->BSRR = ((K_R0 | K_R1 | K_R2 | K_R3) << 16);
+	k_col = (GPIOD->IDR & 0x0f00) >> 8;	// 0000 CCCC 0000 RRRR
+	if (k_col == 0x0f) {
+		return 0;	// No key pressed
+	}
+	while (1) {
+		k_row = 0;
+		GPIOD->BSRR = (K_R0 | K_R1 | K_R2 | K_R3); // R0-R3=1
+		GPIOD->BSRR = (K_R0 << 16);	// R0=0
+		delay_ms(10);
+		k_col = (GPIOD->IDR & 0x0f00) >> 8;	// 0000 CCCC 0000 RRRR
+		if (k_col != 0x0f) {
+			break;
+		}
+		k_row = 1;
+		GPIOD->BSRR = (K_R0 | K_R1 | K_R2 | K_R3); // R0-R3=1
+		GPIOD->BSRR = (K_R1 << 16);	// R1=0
+		delay_ms(10);
+		k_col = (GPIOD->IDR & 0x0f00) >> 8;	// 0000 CCCC 0000 RRRR
+		if (k_col != 0x0f) {
+			break;
+		}
+		k_row = 2;
+		GPIOD->BSRR = (K_R0 | K_R1 | K_R2 | K_R3); // R0-R3=1
+		GPIOD->BSRR = (K_R2 << 16);	// R1=0
+		delay_ms(10);
+		k_col = (GPIOD->IDR & 0x0f00) >> 8;	// 0000 CCCC 0000 RRRR
+		if (k_col != 0x0f) {
+			break;
+		}
+		k_row = 3;
+		GPIOD->BSRR = (K_R0 | K_R1 | K_R2 | K_R3); // R0-R3=1
+		GPIOD->BSRR = (K_R3 << 16);	// R1=0
+		delay_ms(10);
+		k_col = (GPIOD->IDR & 0x0f00) >> 8;	// 0000 CCCC 0000 RRRR
+		if (k_col != 0x0f) {
+			break;
+		}
+		return 0;	// No key pressed (not expected)
+	}
+	if (k_col == 0x0E) {	// 1110
+		return keymap[k_row][0];
+	}
+	if (k_col == 0x0D) {	// 1101
+		return keymap[k_row][1];
+	}
+	if (k_col == 0x0B) {	// 1011
+		return keymap[k_row][2];
+	}
+	if (k_col == 0x07) {	// 0111
+		return keymap[k_row][3];
+	}
+	return 0;
+}
+
 void GPIO_Init(void) {
+	// PORTA User Button
     // Enable clock to GPIO Port
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;            // enable clock for GPIOA (button)
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;            // enable clock for GPIOD (LEDs)
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;            // enable clock for GPIOE (HD44780)
 
-    // Set GPIO pins as output pins
-    // Enable GPIO pins GPIOD15,14,13,12: 01 01 01 01 0x55000000
-								// 15141312 11100908 07060504 03020100
-	GPIOD->MODER = 0x55000000; 	// 01010101 00000000 00000000 00000000
+	// These bits are written by software to configure the I/O direction mode.
+	// 00: Input (reset state)
+	// 01: General purpose output mode
+	// All inputs
+	// PORTA User Button is input
+	// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+	//                                              UB
+	// 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	//   0     0     0     0     0     0     0     0
+//	GPIOA->MODER = 0x00000000;
+//	GPIOA->OSPEEDR = 0;
+	//	These bits are written by software to configure the output type of the I/O port.
+	//	0: Output push-pull (reset state)
+	//	1: Output open-drain
+//	GPIOA->OTYPER = 0;
+//	GPIOA->PUPDR = 0;
+
+	// Now, lock GPIOA configuration
+//	GPIOA->LCKR = (USER_BUTTON | LCKK);
+//	GPIOA->LCKR = (USER_BUTTON);
+//	GPIOA->LCKR = (USER_BUTTON | LCKK);
+//	uint32_t lockedA = GPIOA->LCKR;
+//	lockedA = GPIOA->LCKR;	// 2nd read needed, as the LCKK bit is updated after the first read
+
+	// PORTD KEYPAD + LEDs
+    // Enable clock to GPIO Port
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;            // enable clock for GPIOD (LEDs)
+	
+	// These bits are written by software to configure the I/O direction mode.
+	// 00: Input (reset state)
+	// 01: General purpose output mode
+	// PORTD KEYPAD + LEDs: LEDs are outputs, Keypad Colums are inputs, Keypad Rows are outputs
+	// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+	// LB LR LO LG C3 C2 C1 C0       LE    R3 R2 R1 R0
+	// 01 01 01 01 00 00 00 00 00 00 01 00 01 01 01 01
+	//   5     5     0     0     0     4     5     5
+	GPIOD->MODER = 0x55000455;
 	GPIOD->OSPEEDR = 0;
-	GPIOD->OTYPER = 0;
-	GPIOD->PUPDR = 0;
+	//	These bits are written by software to configure the output type of the I/O port.
+	//	0: Output push-pull (reset state)
+	//	1: Output open-drain
+	// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+	// LB LR LO LG C3 C2 C1 C0       LE    R3 R2 R1 R0
+	//  0  0  0  0  0  0  0  0  0  0  0  0  1  1  1  1
+	//       0           0           0           f
+	GPIOD->OTYPER = 0x000f;
+	//	These bits are written by software to configure the I/O pull-up or pull-down
+	//	00: No pull-up, pull-down
+	//	01: Pull-up
+	//	10: Pull-down	
+	// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+	// LB LR LO LG C3 C2 C1 C0       LE    R3 R2 R1 R0
+	// 00 00 00 00 01 01 01 01 00 00 00 00 00 00 00 00
+	//   0     0     5     5     0     0     0     0
+	GPIOD->PUPDR = 0x00550000;
 
 	// Now, lock GPIOD configuration
-	GPIOD->LCKR = (LED_LD5_R | LED_LD4_G | LED_LD6_B | LED_LD3_O | LED_LD8_R | LCKK);
-	GPIOD->LCKR = (LED_LD5_R | LED_LD4_G | LED_LD6_B | LED_LD3_O | LED_LD8_R);
-	GPIOD->LCKR = (LED_LD5_R | LED_LD4_G | LED_LD6_B | LED_LD3_O | LED_LD8_R | LCKK);
-	uint32_t lockedD = GPIOD->LCKR;
-	lockedD = GPIOD->LCKR;	// 2nd read needed, as the LCKK bit is updated after the first read
+//	GPIOD->LCKR = (LED_LD5_R | LED_LD4_G | LED_LD6_B | LED_LD3_O | LED_LD8_R | LCKK);
+//	GPIOD->LCKR = (LED_LD5_R | LED_LD4_G | LED_LD6_B | LED_LD3_O | LED_LD8_R);
+//	GPIOD->LCKR = (LED_LD5_R | LED_LD4_G | LED_LD6_B | LED_LD3_O | LED_LD8_R | LCKK);
+//	uint32_t lockedD = GPIOD->LCKR;
+//	lockedD = GPIOD->LCKR;	// 2nd read needed, as the LCKK bit is updated after the first read
+
+	// PORTE LCD
+    // Enable clock to GPIO Port
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;            // enable clock for GPIOE (HD44780)
 
 #ifdef DISPLAY_4_BIT_MODE
-// Discovery STM32F4
-// Use the display in 4-bit mode
-// PORTE
-// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
-// D7 D6 D5 D4 -- -- -- -- RW RS  E
-// 01 01 01 01 00 00 00 00 01 01 01 00 00 00 00 00
-//   5     5     0     0     5     4     0     0
+	// Discovery STM32F4
+	// Use the display in 4-bit mode
+	// These bits are written by software to configure the I/O direction mode.
+	// 00: Input (reset state)
+	// 01: General purpose output mode
+	// PORTE display
+	// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+	// D7 D6 D5 D4 -- -- -- -- RW RS  E
+	// 01 01 01 01 00 00 00 00 01 01 01 00 00 00 00 00
+	//   5     5     0     0     5     4     0     0
 	GPIOE->MODER = 0x55005400;
-	GPIOE->OSPEEDR = 0;
-	GPIOE->OTYPER = 0;
-	GPIOE->PUPDR = 0;
 #else
-// Discovery STM32F4
-// Use the display in 8-bit mode
-// PORTE
-// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
-// D7 D6 D5 D4 D5 D6 D7 D8 RW RS  E
-// 01 01 01 01 01 01 01 01 01 01 01 00 00 00 00 00
-//   5     5     5     5     5     4     0     0
+	// Discovery STM32F4
+	// Use the display in 8-bit mode
+	// These bits are written by software to configure the I/O direction mode.
+	// 00: Input (reset state)
+	// 01: General purpose output mode
+	// PORTE display
+	// 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+	// D7 D6 D5 D4 D5 D6 D7 D8 RW RS  E
+	// 01 01 01 01 01 01 01 01 01 01 01 00 00 00 00 00
+	//   5     5     5     5     5     4     0     0
 	GPIOE->MODER = 0x55555400;
+#endif
 	GPIOE->OSPEEDR = 0;
+	//	These bits are written by software to configure the output type of the I/O port.
+	//	0: Output push-pull (reset state)
+	//	1: Output open-drain
 	GPIOE->OTYPER = 0;
 	GPIOE->PUPDR = 0;
-#endif
-    // Now, lock GPIOE configuration
+
+	// Now, lock GPIOE configuration
 	// 0xffe0 11111111 11100000
 //	GPIOE->LCKR = 0xffe0 | LCKK;
 //	GPIOE->LCKR = 0xffe0;
@@ -298,8 +499,8 @@ void GPIO_Init(void) {
 //	uint32_t lockedE = GPIOE->LCKR;
 //	lockedE = GPIOE->LCKR;	// 2nd read needed, as the LCKK bit is updated after the first read
 
-	// Indicate GPIOs are locked
-//	if ((lockedD & LCKK) && (lockedE & LCKK)) {
+//	// Indicate GPIOs are locked
+//	if ((lockedA & LCKK) && (lockedD & LCKK) && (lockedE & LCKK)) {
 //		GPIOD->BSRR = LED_LD3_O;	// Orange LED indicates successful lock
 //	}
 }
